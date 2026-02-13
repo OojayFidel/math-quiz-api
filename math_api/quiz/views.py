0f1427a0eba36c1be975f0bebc70f060483cc017
OojayFidel <odeyjonathan008@gmail.com>
2026-02-13 15:02:29 +0100

    Implement GET topics endpoint

--- math_api/quiz/views.py
+++ math_api/quiz/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Topic
+from .serializers import TopicSerializer
 
-# Create your views here.
+
+class TopicListView(APIView):
+    def get(self, request):
+        topics = Topic.objects.all()
+        serializer = TopicSerializer(topics, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
